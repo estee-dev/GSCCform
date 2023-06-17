9e94c6ba4c58743e65493bf7d5b553702b4d3847
estee-dev <stanleychimbuzor@gmail.com>
2023-06-17 06:57:48 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+$(document).ready(function(){
 
-    $(function () {
-        $('#datetimepicker1').datetimepicker();
+    $('#submit1').click(function(){
+
+        $('form').hide();
+        $('.popup').show();
+        $('h1').hide();
+        $('p').hide();
+        $('#submit1').hide();
+
+
     });
+})
